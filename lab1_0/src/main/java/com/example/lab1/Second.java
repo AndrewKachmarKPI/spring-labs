8cd0749611490b5c15c719d20214f54e4e78b819
Andrea04172004 <kachmar24@ukr.net>
2023-09-15 08:24:58 +0300

    fix index page

--- lab1_0/src/main/java/com/example/lab1/Second.java
+++ lab1_0/src/main/java/com/example/lab1/Second.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Order(2)
+@Order(3)
 @Component
 public class Second implements CommandLineRunner {
     @Override
