d2991c1ec496d88395ed39a6d798cd75b480fa21
AndriiKachmar <akacmar1@gmail.com>
2023-10-09 15:32:44 +0300

    lab4

---
+++ lab4/src/main/java/com/spring/labs/lab4/Lab4Application.java
@@ -0,0 +1,33 @@
+package com.spring.labs.lab4;
+
+import com.spring.labs.lab4.service.ForumCategoryService;
+import com.spring.labs.lab4.service.UserService;
+import net.datafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+@SpringBootApplication
+public class Lab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(UserService userService, ForumCategoryService forumCategoryService) {
+        return args -> {
+            userService.generateDefaultUsers(100, dataFaker());
+            forumCategoryService.generateDefaultCategories(15, dataFaker());
+        };
+    }
+
+    @Bean
+    @Scope("singleton")
+    Faker dataFaker() {
+        return new Faker();
+    }
+
+}
